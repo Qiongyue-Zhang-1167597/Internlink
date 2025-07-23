554b01ebdaea87ff3edf5099db4aadd1f89a1fef
Qiongyue-Zhang-1167597 <1192651557@qq.com>
2025-07-24 02:07:50 +1200

    Update populate_database.sql
    Replace all customer with student and staff with employer to meet the j requirements

--- LoginExample-v2_0_3 (2)/LoginExample/populate_database.sql
+++ LoginExample-v2_0_3 (2)/LoginExample/populate_database.sql
@@ -1,7 +1,7 @@
 INSERT INTO `users` (`username`, `password_hash`, `email`, `role`)
 VALUES
-	('customer1', '$2b$12$3W3b9AnL6umkIBm6THGHB.yId0/GuR/Gi7R8ZevTwR0xi3o8rrvZa', 'customer1@example.com', 'customer'),
-	('customer2', '$2b$12$6KPIL1c77kZpDXzDDRuJV.ielxGNDUIJPkxl0K5JdQoZTMrcIMBym', 'customer2@example.com', 'customer'),
-	('staff1', '$2b$12$OKt92QtCTJmhr4wqRMYZH.6lOSBUh0oGKcvaNerR2fQe6L7.ViQ0O', 'staff1@example.com', 'staff'),
-	('staff2', '$2b$12$mT8QI2wKBvpoOFZsRwaLkeuS9FS4vc6sX.GJNnNMA1EKmypk6UYGa', 'staff2@example.com', 'staff'),
+	('student1', '$2b$12$3W3b9AnL6umkIBm6THGHB.yId0/GuR/Gi7R8ZevTwR0xi3o8rrvZa', 'student1@example.com', 'student'),
+	('student2', '$2b$12$6KPIL1c77kZpDXzDDRuJV.ielxGNDUIJPkxl0K5JdQoZTMrcIMBym', 'student2@example.com', 'student'),
+	('employer1', '$2b$12$OKt92QtCTJmhr4wqRMYZH.6lOSBUh0oGKcvaNerR2fQe6L7.ViQ0O', 'employer1@example.com', 'employer'),
+	('employer2', '$2b$12$mT8QI2wKBvpoOFZsRwaLkeuS9FS4vc6sX.GJNnNMA1EKmypk6UYGa', 'employer2@example.com', 'employer'),
 	('admin1', '$2b$12$.jUTmK8lb/4O8O3oFuHyeerUl9qs1c.slhGzpYDMJqxGnnToGPuMO', 'admin1@example.com', 'admin');
