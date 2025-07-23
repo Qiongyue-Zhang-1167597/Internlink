2dae5f9fe1dbe00724da0672a3f68039796cdd11
Qiongyue-Zhang-1167597 <1192651557@qq.com>
2025-07-24 02:06:45 +1200

    Update create_database.sql
    Replace all customer with student and staff with employer to meet the j requirements

--- LoginExample-v2_0_3 (2)/LoginExample/create_database.sql
+++ LoginExample-v2_0_3 (2)/LoginExample/create_database.sql
@@ -8,7 +8,7 @@ CREATE TABLE `users` (
   `username` varchar(20) NOT NULL,
   `password_hash` char(60) BINARY NOT NULL COMMENT 'Bcrypt Password Hash and Salt (60 bytes)',
   `email` varchar(320) NOT NULL COMMENT 'Maximum email address length according to RFC5321 section 4.5.3.1 is 320 characters (64 for local-part, 1 for at sign, 255 for domain)',
-  `role` enum('customer','staff','admin') NOT NULL,
+  `role` enum('student','employer','admin') NOT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `username` (`username`)
 )
