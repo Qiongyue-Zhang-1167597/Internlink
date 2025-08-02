509ff5a1657e93c683f02943eededf1257bd9f86
Qiongyue-Zhang-1167597 <1192651557@qq.com>
2025-08-02 19:26:21 +1200

    Create employer.py
    Create protected homepage for employer users
    
    - Implemented the '/employer/home' for the employer dashboard.
    
    - Secured the route using @login_required and @role_required('employer') decorators.
    
    - Fetches the current employer's user data from the database for display on their homepage.

---
+++ LoginExample-v2_0_3 (2)/LoginExample/InternLink/employer.py
@@ -0,0 +1,16 @@
+from InternLink import app
+from InternLink import db
+from flask import redirect, render_template, session, url_for
+from InternLink.utils import login_required, role_required
+
+@app.route('/employer/home')
+@login_required
+@role_required('employer')
+def employer_home():
+    cursor = db.get_db().cursor(dictionary=True)
+    username = session['username']
+    cursor.execute("SELECT * FROM user WHERE username = %s", (username,))
+    user = cursor.fetchone()
+    cursor.close()
+
+    return render_template('employer_home.html', user=user)
