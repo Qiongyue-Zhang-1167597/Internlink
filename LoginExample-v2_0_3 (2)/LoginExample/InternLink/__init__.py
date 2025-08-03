ba9613151318389f56c65067df90e16c55457fd8
Qiongyue-Zhang-1167597 <1192651557@qq.com>
2025-08-03 22:02:34 +1200

    Update __init__.py
    Update the configuration in init.py

--- LoginExample-v2_0_3 (2)/LoginExample/InternLink/__init__.py
+++ LoginExample-v2_0_3 (2)/LoginExample/InternLink/__init__.py
@@ -1,31 +1,27 @@
-# This script runs automatically when our `InternLink` module is first loaded,
-# and handles all the setup for our Flask app.
-from flask import Flask
+
 import os
+from flask import Flask
+from flask_bcrypt import Bcrypt
+
+from . import user, student, admin, employer, db, connect 
+
 
 app = Flask(__name__)
 
-# Replace the default key for security
-app.secret_key = 'KW9dfNV6qB7rWrY5J4aaxeuoqGUE6T532idUGQJaw2U' 
 
-# File upload
+app.secret_key = 'KW9dfNV6qB7rWrY5J4aaxeuoqGUE6T532idUGQJaw2U'
 UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), 'static', 'uploads')
-ALLOWED_EXTENSIONS = {'pdf', 'docx', 'png', 'jpg', 'jpeg'}
-
-# Set upload directory path
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-# Ensure the upload folder exist
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
-# Database connection
-from InternLink import connect
-from InternLink import db
-db.init_db(app, connect.dbuser, connect.dbpass, connect.dbhost, connect.dbname,
-           connect.dbport)
 
-# Include all modules define Flask route-handling function
-from InternLink import user
-from InternLink import student
-from InternLink import employer
-from InternLink import admin
+user.flask_bcrypt.init_app(app)
+
+
+db.init_db(app, connect.dbuser, connect.dbpass, connect.dbhost, connect.dbname, connect.dbport)
+
+
+app.register_blueprint(user.user_bp)
+app.register_blueprint(student.student_bp)
+app.register_blueprint(admin.admin_bp)
+app.register_blueprint(employer.employer_bp) 
